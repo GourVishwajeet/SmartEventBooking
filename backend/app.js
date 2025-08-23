1f0fffd78774e1aa94ea40a1f476d990e5cbe833
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2025-08-23 23:28:42 +0530

    error fixed

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,15 @@ export const io = new Server(server, {
   cors: { origin: ["http://localhost:5173","https://smart-event-booking-dm2h4fw7q-gourvishwajeets-projects.vercel.app","https://smart-event-booking-nine.vercel.app/","https://smart-event-booking-git-main-gourvishwajeets-projects.vercel.app/"] }, // frontend origin
 });
 
-app.use(cors({origin:"http://localhost:5173"}))
+app.use(cors({
+  origin: [
+    "http://localhost:5173",            
+    "https://smart-event-booking-nine.vercel.app" ,
+    "https://smart-event-booking-dm2h4fw7q-gourvishwajeets-projects.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json())
 app.use("/uploads", express.static("uploads"));
 
